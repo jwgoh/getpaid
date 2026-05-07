1f471f986b41aa1d1e1e4cbf12205b1311dd4670
Maksim Pokhiliy <maksim.pokhiliy@gmail.com>
2026-05-07 18:43:42 +0300

    chore: delete features/banking and banking-return page

--- src/app/app/banking-return/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { ConnectReturn } from "@app/features/banking/components";
-
-export default function BankingReturnPage() {
-  return <ConnectReturn />;
-}
--- src/features/banking/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { ConnectBankButton } from "./connect-bank-button";
-export { ConnectReturn } from "./connect-return";
-export { ConnectionList } from "./connection-list";
-export { MatchCard } from "./match-card";
-export { PendingMatches } from "./pending-matches";
